d14ccefa3c075e94ee7afc08d853ebfe4a3599d8
h1jun <dittoavec@gmail.com>
2021-08-11 02:45:44 +0900

    Add router

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest(); 
 const content = document.createElement('div');
-
-// 바뀔 가능성이 있는 데이터는 따로 변수로 빼주는 게 좋다.
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; // id값은 클릭한 컨텐츠에 따라 다르기 때문에 여기서는 마킹만 해놓는다.
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; 
 
 function getData(url) {
     ajax.open('GET', url, false); 
@@ -13,33 +11,49 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL); 
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL); 
+    const newsList = [];
 
-window.addEventListener('hashchange', function() {
-    const id = location.hash.substr(1); // #이후 문자열만 추출
+    newsList.push('<ul>')
 
-    const newsContent = getData( CONTENT_URL.replace('@id', id), false);
-    const title = document.createElement('h1');
+    for(let i = 0; i < 10; i++) {
+        newsList.push( `
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
 
-    title.innerHTML = newsContent.title;
-    content.appendChild(title); 
-    console.log(newsContent);
-});
+    newsList.push('</ul>')
 
-for(let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-    
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
-    `;
-
-    ul.appendChild(div.firstElementChild);
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const id = location.hash.substr(1); // #이후 문자열만 추출
+    const newsContent = getData( CONTENT_URL.replace('@id', id), false);
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+    
+        <div>
+            <a href='#'>목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash; // 화면 전환의 기준 값인 hash 값을 사용 
+    if (routePath === '') { // 첫화면 - 목록, location.hash에 #만 들어 있을 경우 빈 값을 반환한다.
+        newsFeed();
+    } else { // 내용 표시
+        newsDetail();
+    }
+
+}
+window.addEventListener('hashchange', router);
+
+router();
